36cafc828dc415145377d4a92701b2d759034d3a
Adam West <Adamwest023@gmail.com>
2022-11-07 17:34:50 -0600

    created connection.py

--- server/main.py
+++ server/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI, Request
 import uvicorn
 import os
 from dotenv import load_dotenv
+from src.routes.chat import chat
 
 load_dotenv()
 
 api = FastAPI()
+api.include_router(chat)
 
-@api.get("/isitworking")
+@api.get("/test")
 async def root():
     return {"msg": "API is Online"}
 
